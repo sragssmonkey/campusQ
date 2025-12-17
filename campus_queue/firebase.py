d8f8638dd011a034752a1387545e07701bdad8a6
Sragvi Chavan <sragvichavan3954@gmail.com>
2025-12-17 23:27:08 +0530

    Fix Firebase init and static files for production

--- campus_queue/firebase.py
+++ campus_queue/firebase.py
@@ -3,9 +3,14 @@ import json
 import firebase_admin
 from firebase_admin import credentials
 
-if not firebase_admin._apps:
-    firebase_admin.initialize_app(
-        credentials.Certificate(
-            json.loads(os.environ["FIREBASE_ADMIN_JSON"])
-        )
-    )
+def init_firebase():
+    if firebase_admin._apps:
+        return
+
+    firebase_json = os.environ.get("FIREBASE_ADMIN_JSON")
+    if not firebase_json:
+        # Running locally without Firebase admin
+        return
+
+    cred = credentials.Certificate(json.loads(firebase_json))
+    firebase_admin.initialize_app(cred)
